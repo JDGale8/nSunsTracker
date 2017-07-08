8b180bdbbb5d9517a14e8b658721dce6b53986bb
Jake Gale <Jake@JakeGale.com>
2017-07-08 12:57:50 -0700

    add calculator for mondays

--- app/src/main/java/dallasapps/nsunstracker/MondayActivity.java
+++ app/src/main/java/dallasapps/nsunstracker/MondayActivity.java
@@ -3,7 +3,20 @@ package dallasapps.nsunstracker;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
+/**
+ * Controller class for Monday nSuns 4
+ * The user will press each button in order for however many reps they can get
+ * The main buttons will count down from however many reps they start with
+ * The 1+ buttons will count up from 0
+ *
+ * When the user clicks all of them, remove all buttons and display a congratulations post
+ * tell the user whether they should increase their 1RM, or keep it the same
+ *
+ * The user can add accessory excercises (from a searchable list) and select the weight, or body weight, rep, and set count
+ */
 public class MondayActivity extends AppCompatActivity {
 
     @Override
@@ -13,6 +26,35 @@ public class MondayActivity extends AppCompatActivity {
 
 
         final Vibrator vibr = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+
+        // INITIATE BUTTONS
+
+        final Button bench1Btn = (Button) findViewById(R.id.bench1Btn);
+        final Button bench2Btn = (Button) findViewById(R.id.bench2Btn);
+        final Button bench3Btn = (Button) findViewById(R.id.bench3Btn);
+        final Button bench4Btn = (Button) findViewById(R.id.bench4Btn);
+        final Button bench5Btn = (Button) findViewById(R.id.bench5Btn);
+        final Button bench6Btn = (Button) findViewById(R.id.bench6Btn);
+        final Button bench7Btn = (Button) findViewById(R.id.bench7Btn);
+        final Button bench8Btn = (Button) findViewById(R.id.bench8Btn);
+        final Button bench9Btn = (Button) findViewById(R.id.bench9Btn);
+
+        final Button ohp1Btn = (Button) findViewById(R.id.ohp1Btn);
+        final Button ohp2Btn = (Button) findViewById(R.id.ohp2Btn);
+        final Button ohp3Btn = (Button) findViewById(R.id.ohp3Btn);
+        final Button ohp4Btn = (Button) findViewById(R.id.ohp4Btn);
+        final Button ohp5Btn = (Button) findViewById(R.id.ohp5Btn);
+        final Button ohp6Btn = (Button) findViewById(R.id.ohp6Btn);
+        final Button ohp7Btn = (Button) findViewById(R.id.ohp7Btn);
+        final Button ohp8Btn = (Button) findViewById(R.id.ohp8Btn);
+
+        final Button addAccessoryBtn = (Button) findViewById(R.id.addAccessoryBtn);
+
+        final EditText bench1RM = (EditText) findViewById(R.id.bench1RMEditNumber);
+        final EditText ohp1RM = (EditText) findViewById(R.id.ohp1RMEditNumber);
+
+
+
 
     }
 }
---
+++ app/src/main/java/dallasapps/nsunstracker/util/weightCalculator.java
@@ -0,0 +1,88 @@
+package dallasapps.nsunstracker.util;
+
+/**
+ * Created by katy on 7/8/17.
+ */
+
+
+/**
+ * calculates weights for each set
+ * if kg is true, round to nearest 2.5, if kg is false, we're in lbs and round to nearest 5
+ */
+public class weightCalculator {
+
+    private boolean isKg;
+    private double roundingFactor;
+
+    weightCalculator(boolean isKg) {
+        calculateRoundingFactor();
+    }
+
+    public void calculateRoundingFactor() {
+        if(isKg) {
+            this.roundingFactor = 2.5;
+        }
+        else {
+            this.roundingFactor = 5.0;
+        }
+
+    }
+    public void setIsKg(boolean isKg) {
+        this.isKg = isKg;
+        calculateRoundingFactor();
+    }
+
+    public double calculateBench1(double oneRepMax, boolean kg) {
+        return (oneRepMax *  0.65) / roundingFactor * roundingFactor;
+    }
+    public double calculateBench2(double oneRepMax) {
+        return (oneRepMax *  0.75) / roundingFactor * roundingFactor;
+    }
+    public double calculateBench3(double oneRepMax) {
+        return (oneRepMax *  0.85) / roundingFactor * roundingFactor;
+    }
+    public double calculateBench4(double oneRepMax) {
+        return (oneRepMax *  0.85) / roundingFactor * roundingFactor;
+    }
+    public double calculateBench5(double oneRepMax) {
+        return (oneRepMax *  0.85) / roundingFactor * roundingFactor;
+    }
+    public double calculateBench6(double oneRepMax) {
+        return (oneRepMax *  0.8) / roundingFactor * roundingFactor;
+    }
+    public double calculateBench7(double oneRepMax) {
+        return (oneRepMax *  0.75) / roundingFactor * roundingFactor;
+    }
+    public double calculateBench8(double oneRepMax) {
+        return (oneRepMax *  0.7) / roundingFactor * roundingFactor;
+    }
+    public double calculateBench9(double oneRepMax) {
+        return (oneRepMax *  0.65) / roundingFactor * roundingFactor;
+    }
+    
+    public double calculateOhp1(double oneRepMax) {
+        return (oneRepMax * 0.5) / roundingFactor * roundingFactor;
+    }
+    public double calculateOhp2(double oneRepMax) {
+        return (oneRepMax * 0.6) / roundingFactor * roundingFactor;
+    }
+    public double calculateOhp3(double oneRepMax) {
+        return (oneRepMax * 0.7) / roundingFactor * roundingFactor;
+    }
+    public double calculateOhp4(double oneRepMax) {
+        return (oneRepMax * 0.7) / roundingFactor * roundingFactor;
+    }
+    public double calculateOhp5(double oneRepMax) {
+        return (oneRepMax * 0.7) / roundingFactor * roundingFactor;
+    }
+    public double calculateOhp6(double oneRepMax) {
+        return (oneRepMax * 0.7) / roundingFactor * roundingFactor;
+    }
+    public double calculateOhp7(double oneRepMax) {
+        return (oneRepMax * 0.7) / roundingFactor * roundingFactor;
+    }
+    public double calculateOhp8(double oneRepMax) {
+        return (oneRepMax * 0.7) / roundingFactor * roundingFactor;
+    }
+
+}

26ad7407a9f69effaf75f7530f44f813bd54a336
JDGale8 <jake@jakegale.com>
2017-07-07 20:37:48 -0700

    add vibrator

--- app/src/main/java/dallasapps/nsunstracker/MondayActivity.java
+++ app/src/main/java/dallasapps/nsunstracker/MondayActivity.java
@@ -1,5 +1,6 @@
 package dallasapps.nsunstracker;
 
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MondayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monday);
+
+
+        final Vibrator vibr = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+        
     }
 }
